52adb56e055daa780c975059ee2a46ae6139122c
laura nikol jimenez rojas <jimenezrojaslauraniokl@gmail.com>
2024-04-02 10:57:48 -0500

    version 1

---
+++ my-project/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/about/about.component';
+import { CatalogoComponent } from './components/catalogo/catalogo.component';
+import { LoginComponent } from './components/login/login.component';
+
+const routes: Routes = [
+  {
+    path: 'catalogo',
+    component: CatalogoComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  { path: 'login', component: LoginComponent
+ }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
